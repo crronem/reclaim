78be860cc420f951227da5af9ad9d32d313067e8
Christopher Richardson <chris@onem.com>
2019-11-27 13:55:00 +0000

    have no idea about these changes

---
+++ app_api/routes/landing.js
@@ -0,0 +1,27 @@
+const logger = require('debug-level')('anycoop')
+
+const ObjectId = require('mongoose').Types.ObjectId
+const moment = require("moment")
+
+const { loadTemplate } = require('onemsdk').parser
+const { Response } = require('onemsdk')
+
+const menu = function () {
+    return async function (req, res) {
+        data = {}
+        try {
+            logger.info("-----showMessage() data------")
+            logger.info(JSON.stringify(data, {}, 4))
+            let rootTag = loadTemplate("./src/app_api/menus/landing.pug", data)
+            let response = Response.fromTag(rootTag)
+            return res.json(response.toJSON())
+        } catch (error) {
+            logger.info("-----Landing() Error------")
+            console.log(error)
+        }
+    }
+}
+
+module.exports = {
+    menu
+}
