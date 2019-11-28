4ccbbe516bb8f87bc9fa5d154a1b370c24a81b28
Christopher Richardson <chris@onem.com>
2019-11-28 11:30:49 +0000

    fixing procfile

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -2,6 +2,9 @@
 const express = require('express')
 const api = express.Router()
 const Landing = require('../routes/landing')
+const Grade = require('../routes/grade')
+const Buy = require('../routes/buy')
+const Sell = require('../routes/sell')
 
 
 // function capitalize(string) {
@@ -15,5 +18,9 @@ const Landing = require('../routes/landing')
 // Main landing menu & new users
 api.get('/', Landing.menu())
 api.get('/landing', Landing.menu())
+api.get('/gradeSelect/:mode', Grade.sellGrade())
+api.get('/grade/:grade/:mode', Grade.grade())
+api.get('/buyDataCenter', Buy.buyDataCenter())
+api.get('/sellDataCenter', Buy.sellDataCenter())
 
 module.exports = api
